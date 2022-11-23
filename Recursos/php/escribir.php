a295a0f9bd4342ac05dbab6df32e1452454a92f0
plh00004 <plh00004@red.ujaen.es>
2022-11-23 13:38:19 +0100

    commit limpieza codigo
    Eliminación ficheros redundantes para la correcta ejecución de la interfaz

--- Recursos/php/escribir.php
+++
@@ -1,9 +0,0 @@
-<?php
-////////////////// CONEXION A LA BASE DE DATOS ////////////////////////////////////
-
-$datos = $_REQUEST['modelo'];
-$ar=fopen("../Modelos/archivo.txt","a") or die ("Error al crear");
-
-fwrite($ar,$datos);
-fclose($ar);
-
